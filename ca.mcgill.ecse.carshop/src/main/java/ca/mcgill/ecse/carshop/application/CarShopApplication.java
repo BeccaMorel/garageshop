8275fe717a77697af218b2f907582c72a3159b2e
emile-bc <45125726+emile-bc@users.noreply.github.com>
2021-06-07 01:41:15 -0400

    Added basic CLI

--- ca.mcgill.ecse.carshop/src/main/java/ca/mcgill/ecse/carshop/application/CarShopApplication.java
+++ ca.mcgill.ecse.carshop/src/main/java/ca/mcgill/ecse/carshop/application/CarShopApplication.java
@@ -4,23 +4,133 @@
 package ca.mcgill.ecse.carshop.application;
 
 import java.util.List;
+import java.util.Scanner;
 
 import ca.mcgill.ecse.carshop.model.*;
 public class CarShopApplication {
     public String getGreeting() {
-        return "Hello World!";
+        return "Welcome to The Car Shop!";
     }
 
     public static void main(String[] args) {
         System.out.println(new CarShopApplication().getGreeting());
+        
+        //configure the car shop
+        setup();
+        
+        //for prompts
+        boolean running = true;
+        Scanner scan = new Scanner(System.in);
+        
+        while(running)
+        {
+        	if(!isLoggedIn())
+        	{
+	            System.out.println("\nThe Car Shop Main Menu");
+	            System.out.println("\t\t1. Login");
+	            System.out.println("\t\t2. Signup");
+	            System.out.println("\t\t3. Quit");
+	            System.out.print("Please Enter Your Option: ");
+        	}
+        	else
+        	{
+        		System.out.println("\nWelcome, " + activeUser.getUsername());
+	            System.out.println("\t\t1. View/Update Appointment");
+	            System.out.println("\t\t2. Book Appointment");
+	            System.out.println("\t\t3. Quit");
+	            System.out.print("Please Enter Your Option: ");
+        	}        	
+
+			int choice = 0;
+			try{
+				choice = scan.nextInt();
+			}
+			catch(Exception e)
+			{
+				System.out.println("Invalid option..");
+				continue;
+			}
+
+            switch(choice)
+            {
+				// Login or view/update appointment
+                case 1:
+                {
+                	if(!isLoggedIn())
+                	{
+						System.out.print("Please Enter Your Username: ");
+						String username = scan.next();
+						
+						System.out.print("Please Enter Your Password: ");
+						String password = scan.next();
+						
+						System.out.println(CarShopApplication.login(username, password));
+                	}
+                	else
+                	{
+                		if(activeUser.getAppointments().size() == 0)
+                		{
+                			System.out.println("You have no appointments");
+                		}
+                		else
+                		{
+                			System.out.println(activeUser.getAppointment(0));
+
+                			System.out.print("Update? (y/n): ");
+    						if(scan.next().equals("y"))
+    						{
+    							System.out.print("PROMPT FOR UPDATE");
+    						}
+                		}
+                		
+                	}
+
+                    break;
+                }
+				// Signup or book appointment
+                case 2:
+                {
+                	if(!isLoggedIn())
+                	{
+                		System.out.print("Please Select a Username: ");
+                	}
+                	else
+                	{
+                		System.out.print("PROMPT FOR BOOKING");
+                	}
+					
+                    break;
+                }
+				// Exit Application
+                case 3:
+                {
+                    running = false;
+                    scan.close();
+                    activeUser = null;
+                    theCarShop.delete();
+                    System.out.println("Thank you for using The Car Shop!");
+                    break;
+                }
+				// Invalid
+                default:
+                {
+                    System.out.println("Invalid option \'" + choice + "\'");
+                    break;
+                }
+            }
+        }        
     }
     
     /***Make the basic car shop***/
     static CarShop theCarShop = new CarShop();
     static Customer activeUser = null;
     
-    /***Retrieve the basic car shop***/
+    private static void setup()
+    {
+    	theCarShop.addCustomer(new Customer("user", "test", theCarShop));
+    }
     
+    /***Retrieve the basic car shop***/
     public static CarShop getCarshop() {
     	
 		return theCarShop;
